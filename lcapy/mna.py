bd70542d78fb05eadd1564fd8784f13e133ccb5c
Amirmahmoud Rashidi <rashidi.amirmahmood@gmail.com>
2026-08-13 15:33:50 +0330

    Update mna.py

--- lcapy/mna.py
+++ lcapy/mna.py
@@ -9,8 +9,8 @@ Copyright (C) 2026 Amirmahmoud Rashidi — modifications
 
 This file is part of lcapy (https://github.com/mph-/lcapy) and is licensed under
 the GNU Lesser General Public License v2.1. See the LICENSE file in project root.
-
 """
+
 from __future__ import division
 from .assumptions import Assumptions
 from .vector import Vector
